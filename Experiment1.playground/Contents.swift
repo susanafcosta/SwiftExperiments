a846c6ac34a351c7afec6292fd2a5361290e8280
Susana Costa <susanacosta@susanas-air.home>
2016-01-24 01:10:12 +0000

    number of ocurrences

--- Experiment1.playground/Contents.swift
+++ Experiment1.playground/Contents.swift
@@ -2,3 +2,32 @@
 
 import UIKit
 
+let listNumbers: [Int] = [1,2,4,8,10,20,30,2,1,10];
+
+func numberOfOcurrences(array: [Int]) -> [(Int,Int)] {
+    
+    var ocurrences: [Int: Int] = [:]
+    var results: [(Int,Int)] = []
+    
+    for number in array {
+        
+        if let innerValue = ocurrences[number] {
+            
+            ocurrences[number] = innerValue + 1
+        }
+        else  {
+            ocurrences[number] = 0
+        }
+    }
+    
+    for key in ocurrences.keys {
+        
+        ocurrences[key].map {
+            results.append((key,$0))
+        }
+    }
+
+    return results
+}
+
+numberOfOcurrences(listNumbers)
